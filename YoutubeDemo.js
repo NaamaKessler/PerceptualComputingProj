0a7534f2a767809cc705598635064e1d4ff7cb82
noystl <noystll@gmail.com>
2019-05-29 12:41:17 +0300

    Putting things in order - midwork

--- YoutubeDemo.js
+++ YoutubeDemo.js
@@ -5,7 +5,7 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
+// 3. This function creates an <iframe> (and YouTube poseDemo)
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
@@ -20,14 +20,14 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
-// 4. The API will call this function when the video player is ready.
+// 4. The API will call this function when the video poseDemo is ready.
 function onPlayerReady(event) {
     event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
+// 5. The API calls this function when the poseDemo's state changes.
 //    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
+//    the poseDemo should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
     // if (event.data == YT.PlayerState.PLAYING && !done) {
