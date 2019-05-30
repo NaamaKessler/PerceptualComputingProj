7d7bd9325747bf39540dc04e96127a4b3ebf3fc6
noystl <noystll@gmail.com>
2019-05-30 18:10:37 +0300

    Did some facelift & activation by Om is working

--- player.js
+++ player.js
@@ -1,3 +1,6 @@
+// TASKS:
+// TODO: Why some of the poses are invalid?
+
 //----------------------INIT YOUTUBE---------------------------//
 
 // This code loads the IFrame Player API code asynchronously.
@@ -24,18 +27,6 @@ function onYouTubeIframeAPIReady() {
 let video;
 let poseNet;
 let poses = [];
-let COUNTER = 0;
-let COUNTDOWN = 0;
-
-let omListener = 5;
-let omsDetected = -1;
-let lastWristX;
-let lastWristY;
-let detectedDirections = [];
-// let rightCircleRegex = ;
-// let leftCircleRegex = ;
-
-
 
 /**
  * Sets up the poseNet library:
@@ -137,36 +128,126 @@ function decreseVolume() {
 
 //----------------------------POSE DETECTION--------------------------------//
 
-function detectOm(pose) {
-    eyes_dist = Math.sqrt(Math.pow((pose.keypoints[1].position.y - pose.keypoints[2].position.y), 2) +
-        Math.pow((pose.keypoints[1].position.x - pose.keypoints[2].position.x), 2));
-//
-    ears_dist = Math.sqrt(Math.pow((pose.keypoints[3].position.y - pose.keypoints[4].position.y), 2) +
-        Math.pow((pose.keypoints[3].position.x - pose.keypoints[4].position.x), 2));
-//
-    wrist_dist = Math.sqrt(Math.pow((pose.keypoints[left_wrist].position.y - pose.keypoints[right_wrist].position.y), 2) +
-        Math.pow((pose.keypoints[left_wrist].position.x - pose.keypoints[right_wrist].position.x), 2));
+//--------------CONSTANTS:
 
-    if(pose.keypoints[left_elbow].score > 0.6 && pose.keypoints[right_elbow].score > 0.6){
-        if (Math.abs(pose.keypoints[left_elbow].position.y - pose.keypoints[right_elbow].position.y) < eyes_dist) {   // elbow's hight
-            if (pose.keypoints[left_wrist].score > 0.3 && pose.keypoints[right_wrist].score > 0.3 && wrist_dist < 1.5*ears_dist) {
-                if (pose.keypoints[right_wrist].position.x > pose.keypoints[right_elbow].position.x &&
-                    pose.keypoints[left_wrist].position.x < pose.keypoints[left_elbow].position.x) {
-                    COUNTER++;
-                    if (COUNTER === 10) {
-                        console.log("Hoooo yeee pose detected!!!");
-                        COUNTER = 0;
-                        COUNTDOWN = 30;
-                        omsDetected += 1;
-                        return true;
-                    }
-                }
-            }
+// Key points
+const LEFT_ELBOW = 7;
+const RIGHT_ELBOW = 8;
+const LEFT_WRIST = 9;
+const RIGHT_WRIST = 10;
+const RIGHT_EYE = 2;
+const LEFT_EYE = 1;
+const RIGHT_EAR = 4;
+const LEFT_EAR = 3;
+
+// Thresholds
+const WRIST_THRESH = 0.3;
+const ELBOW_THRESH = 0.6;
+
+// Pose sensitivity:
+const SLEEP_TIME = 30;
+const OM_SENSITIVITY = 10;  // Determines how many Oms in a row we consider as a true Om (not noise)
+const LISTENING_TIME = 70;
+
+
+//---------------GLOBALS
+
+let counter = 0;
+let countdown = 0;
+let listeningTime = LISTENING_TIME;
+let omsDetected = 0;
+let lastWristX;
+let lastWristY;
+let detectedDirections = [];
+// let rightCircleRegex = ;
+// let leftCircleRegex = ;
+
+
+//---------------FUNCTIONS
+/**
+ * Calculates the Euclidean distance between two key points in a pose object.
+ * @param pose
+ * @param keyPoint1
+ * @param keyPoint2
+ */
+function euclidDist(pose, keyPoint1, keyPoint2){
+    return Math.sqrt(Math.pow((pose.keypoints[keyPoint1].position.y - pose.keypoints[keyPoint2].position.y), 2) +
+        Math.pow((pose.keypoints[keyPoint1].position.x - pose.keypoints[keyPoint2].position.x), 2));
+}
+
+/**
+ * Tests if two key points are in the same height.
+ * @param pose
+ * @param keyPoint1
+ * @param keyPoint2
+ * @param errThresh
+ */
+function sameHeight(pose, keyPoint1, keyPoint2, errThresh){
+    return Math.abs(pose.keypoints[keyPoint1].position.y - pose.keypoints[keyPoint2].position.y) < errThresh;
+}
+
+/**
+ * Returns true if the score of a key point is above the given threshold.
+ * @param pose
+ * @param keyPoint
+ * @param threshold
+ */
+function checkScore(pose, keyPoint, threshold){
+    return pose.keypoints[keyPoint].score > threshold;
+}
+
+/**
+ * Tests if the elbows were detected with high confidence and have in the same height.
+ * @param pose
+ * @param errThresh
+ * @returns {*}
+ */
+function elbowsAligned(pose, errThresh){
+    return checkScore(pose, LEFT_ELBOW, ELBOW_THRESH) && checkScore(pose, RIGHT_ELBOW, ELBOW_THRESH) &&
+        sameHeight(pose, LEFT_ELBOW, RIGHT_ELBOW, errThresh);
+}
+
+/**
+ * Tests if the waists were detected with high confidence, and if they are close.
+ * @param pose
+ * @param errThresh
+ */
+function closeWrists(pose, errThresh){
+    var wrist_dist = euclidDist(pose, LEFT_WRIST, RIGHT_WRIST);
+    return checkScore(pose, LEFT_WRIST, WRIST_THRESH) &&
+        checkScore(pose, RIGHT_WRIST, WRIST_THRESH) && wrist_dist < errThresh;
+}
+
+/**
+ * Tests if the wrists are inner compared to the elbows.
+ * @param pose
+ * @returns {boolean}
+ */
+function wristsInwards(pose){
+    return pose.keypoints[RIGHT_WRIST].position.x > pose.keypoints[RIGHT_ELBOW].position.x &&
+        pose.keypoints[LEFT_WRIST].position.x < pose.keypoints[LEFT_ELBOW].position.x;
+}
+
+/**
+ * Tests if the pose is the Om pose.
+ * @param pose
+ * @returns {boolean}
+ */
+function detectOm(pose) {
+    let eyes_dist = euclidDist(pose, LEFT_EYE, RIGHT_EYE);
+    let ears_dist = euclidDist(pose, LEFT_EAR, RIGHT_EAR);
+
+    if(elbowsAligned(pose, eyes_dist) && closeWrists(pose, 1.5*ears_dist) && wristsInwards(pose)){
+        counter++;
+        if (counter === OM_SENSITIVITY) { // If we detected enough Oms, its probably not a noise.
+            counter = 0;
+            countdown = SLEEP_TIME; // Do not detect a spacial pose for the next SLEEP_TIME iterations.
+            omsDetected++;
+            return true;
         }
     }
     return false;
 }
-
 
 /**
  * This function inspects the current pose and checks if its a spacial pose.
@@ -175,78 +256,78 @@ function detectOm(pose) {
 function poseDetection() {
     for (let i = 0; i < poses.length; i++) {
         let pose = poses[i].pose;
+
+        // Tests if the pose is valid:
         if (!pose) {
             continue;
         }
-        left_elbow = 7;
-        right_elbow = 8;
-        left_wrist = 9;
-        right_wrist = 10;
-        eyes_dist = Math.sqrt(Math.pow((pose.keypoints[1].position.y - pose.keypoints[2].position.y), 2) +
-            Math.pow((pose.keypoints[1].position.x - pose.keypoints[2].position.x), 2));
-//
-        ears_dist = Math.sqrt(Math.pow((pose.keypoints[3].position.y - pose.keypoints[4].position.y), 2) +
-            Math.pow((pose.keypoints[3].position.x - pose.keypoints[4].position.x), 2));
-//
-        wrist_dist = Math.sqrt(Math.pow((pose.keypoints[left_wrist].position.y - pose.keypoints[right_wrist].position.y), 2) +
-            Math.pow((pose.keypoints[left_wrist].position.x - pose.keypoints[right_wrist].position.x), 2));
-//
-        if (COUNTDOWN > 0) {
-            COUNTDOWN -=1;
+
+        // If we just detected a pose, the current pose is probably trash, so move on:
+        if (countdown > 0) {
+            countdown--;
             return;
         }
-        if (omsDetected === -1) {
-            console.log("Starting");
+
+        // Waits for activation:
+        if (omsDetected === 0) {
+            console.log("Waits for activation");
             detectOm(pose);
             continue;
         }
 
-        if (omsDetected === 1){
+        // After activated, listens for the next command:
+        if (listeningTime >= 0) {
             console.log("listening");
-            if (omListener >= 0) {
-                if (detectOm(pose)) {
-                    if (player.getPlayerState() !== 1) { // start playing
-                        playVid();
-                    } else { // stop playing
-                        player.pause();
-                    }
-                    omsDetected = 0;
-                } else {
-                    // detect circle
-                    x_delta = pose.keypoints[right_wrist].position.x - lastWristX;
-                    y_delta = pose.keypoints[right_wrist].position.y - lastWristY;
-                    if (Math.abs(x_delta) > Math.abs(y_delta)) { // left-right movement
-                        if (x_delta > 0) { //left
-                            if (detectedDirections[detectedDirections.length - 1] !== 'l') {
-                                detectedDirections.push('l');
-                            }
-                        } else { //right
-                            if (detectedDirections[detectedDirections.length - 1] !== 'r') {
-                                detectedDirections.push('r');
-                            }
-                        }
-                    } else { // up-down movement
-                        if (y_delta > 0) { // down
-                            if (detectedDirections[detectedDirections.length - 1] !== 'd') {
-                                detectedDirections.push('d');
-                            }
-                        } else { // up
-                            if (detectedDirections[detectedDirections.length - 1] !== 'u') {
-                                detectedDirections.push('u');
-                            }
-                        }
-                    }
+            if (detectOm(pose)) {
+                console.log("detected Om while listening.")
+                if (player.getPlayerState() !== 1) { // start playing
+                    playVid();
+                } else { // stop playing
+                    pauseVid();
                 }
-                omListener -= 1;
             }
-            if (omListener === 0) { // end of listening period
-                // todo - check regex to see if there is a circle
-                omsDetected = 0;
-            }
-
+            listeningTime--;
+        } else { // End of listening time.
+            omsDetected = 0;
+            listeningTime = LISTENING_TIME;
         }
-        // update last coordinates:
-        lastWristX = pose.keypoints[right_wrist].position.x;
-        lastWristY = pose.keypoints[right_wrist].position.y;
     }
 }
+
+// Circle Detection:
+//     } else {
+//         // detect circle
+//         x_delta = pose.keypoints[RIGHT_WRIST].position.x - lastWristX;
+//         y_delta = pose.keypoints[RIGHT_WRIST].position.y - lastWristY;
+//         if (Math.abs(x_delta) > Math.abs(y_delta)) { // left-right movement
+//             if (x_delta > 0) { //left
+//                 if (detectedDirections[detectedDirections.length - 1] !== 'l') {
+//                     detectedDirections.push('l');
+//                 }
+//             } else { //right
+//                 if (detectedDirections[detectedDirections.length - 1] !== 'r') {
+//                     detectedDirections.push('r');
+//                 }
+//             }
+//         } else { // up-down movement
+//             if (y_delta > 0) { // down
+//                 if (detectedDirections[detectedDirections.length - 1] !== 'd') {
+//                     detectedDirections.push('d');
+//                 }
+//             } else { // up
+//                 if (detectedDirections[detectedDirections.length - 1] !== 'u') {
+//                     detectedDirections.push('u');
+//                 }
+//             }
+//         }
+//     }
+//     listeningTime -= 1;
+// }
+// if (listeningTime === 0) { // end of listening period
+//     // todo - check regex to see if there is a circle
+//     omsDetected = 0;
+// }
+
+// // update last coordinates:
+// lastWristX = pose.keypoints[right_wrist].position.x;
+// lastWristY = pose.keypoints[right_wrist].position.y;
