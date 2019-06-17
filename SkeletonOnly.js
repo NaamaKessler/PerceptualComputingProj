8c34f6d65bad94bb1086b6ab6a222505daac427f
noystl <noystll@gmail.com>
2019-06-17 09:44:43 +0300

    Removed some comments

--- SkeletonOnly.js
+++ SkeletonOnly.js
@@ -2,25 +2,6 @@
 let listeningBar = new ldBar("#myItem1");
 let musicBar = new ldBar("#musicBar");
 
-// let audioPlayer = document.querySelector('.green-audio-player');
-// let speaker = audioPlayer.querySelector('#speaker');
-// let volumeBtn = audioPlayer.querySelector('.volume-btn');
-// let volumeControls = audioPlayer.querySelector('.volume-controls');
-// let volumeProgress = volumeControls.querySelector('.slider .progress');
-//
-// player.addEventListener('volumechange', updateVolume);
-//
-// function updateVolume() {
-//     volumeProgress.style.height = player.volume * 100 + '%';
-//     if(player.volume >= 0.5) {
-//         speaker.attributes.d.value = 'M14.667 0v2.747c3.853 1.146 6.666 4.72 6.666 8.946 0 4.227-2.813 7.787-6.666 8.934v2.76C20 22.173 24 17.4 24 11.693 24 5.987 20 1.213 14.667 0zM18 11.693c0-2.36-1.333-4.386-3.333-5.373v10.707c2-.947 3.333-2.987 3.333-5.334zm-18-4v8h5.333L12 22.36V1.027L5.333 7.693H0z';
-//     } else if(player.volume < 0.5 && player.volume > 0.05) {
-//         speaker.attributes.d.value = 'M0 7.667v8h5.333L12 22.333V1L5.333 7.667M17.333 11.373C17.333 9.013 16 6.987 14 6v10.707c2-.947 3.333-2.987 3.333-5.334z';
-//     } else if(player.volume <= 0.05) {
-//         speaker.attributes.d.value = 'M0 7.667v8h5.333L12 22.333V1L5.333 7.667';
-//     }
-// }
-
 function hideImage(id) {
     let img = document.getElementById(id);
     img.style.visibility = 'hidden';
